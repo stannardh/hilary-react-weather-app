263e54e7fd74ad76e4b45e0192c9efac285a2eb5
stannardh <hilaryvstannard@gmail.com>
2020-12-26 21:08:41 +0100

    added location button

--- src/LocationButton.js
+++ src/LocationButton.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./Weather.css";
 
-export default function LocationButton() {
-  return <button id="locationButton">Current Location</button>;
+
+export default function LocationButton(response) {
+ return <button id="locationButton">Current Location</button>
+
 }
+
+function handlePosition(position) {
+  
+
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+}
+
+navigator.geolocation.getCurrentPosition(handlePosition)
